d181ab56800b0f41e290ef554c3d947266988341
John E <johnelewis92@gmail.com>
2017-05-27 14:00:55 -0500

    Final

--- Calculator_TM.py
+++ Calculator_TM.py
@@ -1,7 +1,7 @@
 #This program makes a simple calculator that can add, subtract, multiply and divide
 
 import sys
-import math
+from math import sqrt
 
 class Calculator:
 	
@@ -24,17 +24,16 @@ class Calculator:
 	def divide(self,x, y):
 		return x / y;
 	#Square one integer	
-	def square(x):
+	def square(self,x):
 		return x * x;
 	#Multiplicative Inverse of one integer
-	def multinv(x):
+	def multinv(self,x):
 		try:
 			a = 1 /x
 		except ZeroDivisionError:
 			print '*'*30
 			print "\n\nInvalid input cannot divide by 0.\n\n"
 			print '*'*30
-			continue
 		
 		return 1 / x;
 
@@ -44,13 +43,43 @@ class Calculator:
 
 class Engine:
 	
-	c = Calculator()
+	
+	def twoint(self,m):
+		a = int(raw_input("Enter your first number: \t"))
+		b = int(raw_input("Enter  your second number: \t"))
+		c = Calculator()
+	
+		if m == 1:
+			print "%d + %d = %d" %(a,b,c.add(a,b))
+		
+		elif m == 2:
+			print "%d - %d = %d" %(a,b,c.subtract(a,b))
+		
+		elif m == 3:
+			print "%d * %d = %d" %(a,b,c.multiply(a,b))
+		
+		elif m == 4:
+			print "%d / %d = %d" %(a,b,c.divide(a,b))
+		
+	def oneint(self,m):
+		a = int(raw_input("Enter your number: \t"))
+		c = Calculator()
+	
+		if m == 5:
+			print "%d ^ 2 = %d" %(a,c.square(a)) 
+				
+		elif m == 6:		
+			print "Square root of %d = %d" %(a,sqrt(a))
+		
+		else:
+			print "Reciprocal of %d = %d" %(a,c.multinv(a))
+	
 	
 	def start(self):
-		While True:
+		while True:
 			print"\n\n\n"
 			print '-'*30
-			print"Welcome to Calculator.py! Select your operation. \n 1. Add \n 2. Subtract \n 3. Multiply \n 4. Divide \n"
+			print"Welcome to Calculator.py! Select your operation. \n 1. Add \n 2. Subtract \n 3. Multiply \n 4. Divide"
 			print" 5. Square \n 6. Squareroot \n 7. Reciprocal \n 8. End \n"
 			
 			try:
@@ -64,9 +93,9 @@ class Engine:
 			print '-'*30
 			print"\n\n\n"
 			
-			if not menu in range(1,8)
+			if menu < 1 or 8 < menu:
 				print '*'*30
-				print "\n\nInvalid input please enter a number between 1 & 8.\n\n"
+				print "\n\nInvalid input please enter a number between 1 & 1000.\n\n"
 				print '*'*30
 				continue
 			
@@ -74,42 +103,16 @@ class Engine:
 				print "\n\n Good Bye! \n\n"
 				sys.exit()
 			
-			elif menu in range(1,4):
-				twoint(menu)
+			elif  1 <= menu <= 4:
+				self.twoint(menu)
 		
-			elif menu in range(5,7):
-				oneint(menu,a)
+			elif 5 <= menu <= 7:
+				self.oneint(menu)
 				
 			else:
 				pass
 				
-	def twoint(m)
-		a = int(raw_input("Enter your first number: \t"))
-		b = int(raw_input("Enter  your second number: \t"))
-		
-		if m == 1:
-			print "%d + %d = %d" %(a,b,c.add(a,b))
-		
-		elif m == 2:
-			print "%d - %d = %d" %(a,b,c.subtract(a,b))
-		
-		elif m == 3:
-			print "%d * %d = %d" %(a,b,c.multiply(a,b))
-		
-		elif m == 4:
-			print "%d / %d = %d" %(a,b,c.divide(a,b))
-		
-	def oneint(m)
-		a = int(raw_input("Enter your number: \t"))
-		
-		if m == 5:
-			print "%d ^ 2 = %d" %(a,c.square(a)) 
-				
-		elif m == 6:		
-			print "%Square root of %d = %d" %(a,sqrt(a))
-		
-		else:
-			print "Reciprocal of %d = %d" %(a,c.multinv(a))
+
 
 			
 	
