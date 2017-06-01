8f9d4858fd26b6f419fd97186223d38d46f50916
John E <johnelewis92@gmail.com>
2017-05-31 23:46:00 -0500

    Reorganization

---
+++ Python/Calculator_GUI.py
@@ -0,0 +1,42 @@
+#This program makes a simple calculator that can add, subtract, multiply and divide
+#adding gui using tkinter
+
+
+import math
+from Tkinter import *
+
+class Calculator(object):
+	
+	def __init__(self):
+		pass
+	
+	#Adds two numbers
+	def add(self,x, y):
+		return x + y;
+	
+	#Subtract two integers
+	def subtract(self,x, y):
+		return x - y;
+	
+	#multiply two integers
+	def multiply(self,x, y):
+		return x * y;
+	
+	#Divide two integers
+	def divide(self,x, y):
+		return x / y;
+
+class Engine(object):
+	
+	def start(self):
+		print"\n\n\n"
+		print '-'*30
+		menu = int(raw_input("\n\nWelcome to Calculator.py!\n\n"))
+		print '-'*30
+		print"\n\n\n"
+		c = Calculator()	
+		start()
+			
+	
+begin = Engine()
+begin.start()
